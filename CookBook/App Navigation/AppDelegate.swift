191ebf0221adee6b14f57490253ad76e75e7f573
Zarif Ahmed <ahd.zarif@gmail.com>
2021-06-27 11:43:04 +0530

    Added basic folder structure for the project

---
+++ CookBook/App Navigation/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  CookBook
+//
+//  Created by Zarif Ahmed on 6/27/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ CookBook/Features/Dashboard/DashboardVC.swift
@@ -0,0 +1,19 @@
+//
+//  DashboardVC.swift
+//  CookBook
+//
+//  Created by Zarif Ahmed on 6/27/21.
+//
+
+import UIKit
+
+final class DashboardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
